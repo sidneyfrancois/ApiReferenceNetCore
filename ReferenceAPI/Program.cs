0f17e1345d5381e035f98a195eae1256421c1c07
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-11 17:48:37 -0300

    add DbContext as a service

--- ReferenceAPI/Program.cs
+++ ReferenceAPI/Program.cs
@@ -1,16 +1,13 @@
+using ReferenceAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BlogDataContext>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
