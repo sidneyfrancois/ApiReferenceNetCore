b550a57db5be9cd5c4387f68c0d6e9a13f7d947b
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-11 17:47:01 -0300

    Add project files.

---
+++ ReferenceAPI/Data/BlogDataContext.cs
@@ -0,0 +1,23 @@
+using ReferenceAPI.Data.Mappings;
+using ReferenceAPI.Models;  
+using Microsoft.EntityFrameworkCore;
+
+namespace ReferenceAPI.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source=localhost;Database=BlogDatabase;Integrated Security=True;TrustServerCertificate=True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
+    }
+}
