b5db9da2e173335924c3cc17e337a922e18e9294
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-11 18:34:59 -0300

    cateogry controller to return list of categories

---
+++ ReferenceAPI/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReferenceAPI.Data;
+
+namespace ReferenceAPI.Controllers
+{
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("categories")]
+        public IActionResult Get(
+            [FromServices] BlogDataContext context
+            )
+        {
+            return Ok(context.Categories.ToList());
+        }
+    }
+}
