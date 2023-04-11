13a59ca0d8cebbd18a354c8c30d3d3721963b839
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-11 18:26:32 -0300

    add home controller

---
+++ ReferenceAPI/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReferenceAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
