6a34f462bcf1d2e3222b96271bd8f6905af50115
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-12 09:33:54 -0300

    add constructor and method to ResultViewModel

--- ReferenceAPI/ErrorViewModel/ResultViewModel.cs
+++ ReferenceAPI/ErrorViewModel/ResultViewModel.cs
@@ -2,7 +2,28 @@
 {
     public class ResultViewModel<T>
     {
-        public T Data { get; set; }
+        public ResultViewModel(T data, List<string> errors)
+        {
+            Data = data;
+            Errors = errors;
+        }
+
+        public ResultViewModel(T data)
+        {
+            Data = data;
+        }
+
+        public ResultViewModel(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public ResultViewModel(string error)
+        {
+            Errors.Add(error);
+        }
+
+        public T Data { get; private set; }
         public List<string> Errors { get; private set; } = new();
     }
 }
